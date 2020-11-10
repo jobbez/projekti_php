09bb8902f796b092180819d975231c497ca98c00
Johannes Wahlsten <jobbez.wahlsten@gmail.com>
2020-11-10 10:12:02 +0200

    xD

---
+++ register/register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+require('db.php');
+// If form submitted, insert values into the database.
+if (isset($_REQUEST['username'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username); 
+	$email = stripslashes($_REQUEST['email']);
+	$email = mysqli_real_escape_string($con,$email);
+	$fname = stripslashes($_REQUEST['firstname']);
+	$fname = mysqli_real_escape_string($con,$fname);
+	$lname = stripslashes($_REQUEST['lastname']);
+	$lname = mysqli_real_escape_string($con,$lname);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	$trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (username, password, email, trn_date, firstname, lastname)
+VALUES ('$username', '".md5($password)."', '$email', '$trn_date', '$fname', '$lname')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="text" name="firstname" placeholder="firstname" required />
+<input type="text" name="lastname" placeholder="lastname" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+</body>
+</html>
